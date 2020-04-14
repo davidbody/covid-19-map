cd0cb67dacdb5b0919dfdde5bc6b670f6ae3d097
David W. Body <davidbody@bigcreek.com>
2020-04-13 20:14:08 -0500

    Adjust fips for New York City and Kansas City

--- src/index.js
+++ src/index.js
@@ -5,14 +5,26 @@ import legend from "./legend";
 
 const us_map = async function(data_file) {
   const csv_text = await d3.text(data_file);
-  const csv_data = d3.csvParse(csv_text, ({date, fips, cases}) => [date, fips, +cases]);
+  const csv_data = d3.csvParse(csv_text, ({date, county, fips, cases}) => {
+    if (county === 'New York City') fips = '36061';
+    if (county === 'Kansas City') fips = '29095';
+    return [date, fips, +cases];
+  });
   const last_date = d3.max(csv_data, d => d[0]);
-  const current_data = csv_data.filter(d => d[0] == last_date).map(d => [d[1], d[2]]);
+
+  const current_data = csv_data.filter(d => d[0] === last_date).map(d => [d[1], d[2]]);
+
+  // sum case counts by fips
+  const current_data_summed = d3.nest().
+        key(d => d[0]).
+        rollup(values => d3.sum(values, v => v[1])).
+        entries(current_data).
+        map(e => [e.key, e.value]);
 
   const date_parse = d3.timeParse("%Y-%m-%d");
   const data_date = date_parse(last_date);
   const date_format = d3.timeFormat("%e-%B-%Y");
-  const data = Object.assign(new Map(current_data), {title: "Confirmed US Covid-19 cases"});
+  const data = Object.assign(new Map(current_data_summed), {title: "Confirmed US Covid-19 cases"});
 
   const us = await d3.json("data/counties-albers-10m.json");
   const states = new Map(us.objects.states.geometries.map(d => [d.id, d.properties]));
